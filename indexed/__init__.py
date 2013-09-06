6e71e7e01bbc351e76d4052be48819847c031919
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-09-06 16:54:45 +0200

    Add copyright headers

--- indexed/__init__.py
+++ indexed/__init__.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# indexed.py provides a dictionary that is indexed by insertion order.
+# Copyright (c) 2013 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "Niklas Fiekas"
+__copyright__ = "Copyright 2013, Niklas Fiekas"
+__license__ = "GPL"
+__version__ = "0.0.1"
+__email__ = "niklas.fiekas@tu-clausthal.de"
+__status__ = "Development"
+
 import collections
 import operator
 import sys
